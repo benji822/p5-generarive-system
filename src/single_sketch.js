d2948d713efac3528ea68f9defe490f5151bc83d
Benjamin Ng <benng822@gmail.com>
2022-11-20 19:10:06 +0800

    Setup dev for working with p5.js

---
+++ src/single_sketch.js
@@ -0,0 +1,32 @@
+// project setup
+// draw something simple
+// translate()
+// push() + pop()
+// rotate() + angleMode()
+// rectMode()
+
+import "../css/style.css";
+import { sketch } from "p5js-wrapper";
+import "p5.js-svg";
+
+sketch.setup = function () {
+  createCanvas(800, 600, SVG);
+  noLoop();
+  angleMode(DEGREES);
+  rectMode(CENTER);
+};
+
+sketch.draw = function () {
+  background("teal");
+  fill(0);
+
+  push();
+  translate(width / 2, height / 2);
+  rotate(45);
+  rect(0, 0, 50, 50);
+  pop();
+};
+
+sketch.mousePressed = function () {
+  console.log("here");
+};
